e1826c47ec81908803747af58257b7cb4b816d4b
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-01-23 16:10:36 -0300

    feat: types for jwt and payload

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,20 @@
 import { JWT } from "@fastify/jwt";
 
-declare module "fastify" {
+declare module 'fastify' {
   interface FastifyRequest {
-    jwt: JWT;
+    jwt: JWT
+  }
+  export interface FastifyInstance {
+    authenticate: any
+  }
+}
+type UserPayload = {
+  id: string
+  email: string
+  name: string
+}
+declare module '@fastify/jwt' {
+  interface FastifyJWT {
+    user: UserPayload
   }
 }
